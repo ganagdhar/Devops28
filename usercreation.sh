3923902e7970b4e57a6f6ec006e2256a59a8754d
gangadhar <gkaranapu@cardlytics.com>
2022-09-17 17:51:53 +0530

    changed array

--- usercreation.sh
+++ usercreation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -gt 0 ]; then
     for user in $@; do
-        eu = $(cat /etc/passwd | grep -i -w $user | cut -d ":" -f1)
+        eu=$(cat /etc/passwd | grep -i -w $user | cut -d ":" -f1)
         if [ "${eu}" = "${user}" ]; then
             echo "user with $user already exits.."
         else
