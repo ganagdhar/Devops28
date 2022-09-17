ba6a60bacc040aa0f5b006731a7dcceb670cde75
gangadhar <gkaranapu@cardlytics.com>
2022-09-17 17:48:45 +0530

    changed array

--- usercreation.sh
+++ usercreation.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 if [ $# -gt 0 ]; then
     for user in $@; do
-        echo $user
+        eu = $(cat /etc/passwd | grep -i -w $user | cut -d ":" -f1)
+        if [ "${eu}" = "${user }"]; then
+            echo "user with $user already exits.."
+        else
+            echo "Lets create user $user"
+        fi
     done
 else
     echo "Invalid Input. Please add parameters"
